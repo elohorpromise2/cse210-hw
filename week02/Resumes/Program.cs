dd67138a93b9f442e0dda18fe47208b3663724ab
elohorpromise2 <elohorpromise2@gmail.com>
2026-09-07 15:53:41 +0100

    Complete CSE 210 Week 02 abstraction activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+
+        job1._jobTitle = "CEO";
+        job1._company = "Fashion Castle";
+        job1._startYear = 2019;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+
+        job2._jobTitle = "CEO";
+        job2._company = "Drops and Clouds";
+        job2._startYear = 2026;
+        job2._endYear = 2030;
+
+        Resume myResume = new Resume();
+
+        myResume._name = "Promise Elohor";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
